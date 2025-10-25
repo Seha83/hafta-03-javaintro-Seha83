b2f2259160ceb23e3e5807ece2bba6ffeca78eb4
Seha83 <250541021@firat.edu.tr>
2025-10-25 15:07:52 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,8 @@
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Java'ya Hoş Geldiniz");}
+    }
