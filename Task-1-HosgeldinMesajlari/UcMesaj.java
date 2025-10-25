758298ed3baf54a5c4de6e9e71b51a4f3515cd93
Seha83 <250541021@firat.edu.tr>
2025-10-25 14:54:38 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,9 @@
+package gorev1_hello;
+
+public class UcMesaj {
+    public static void main(String[] args) {
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
